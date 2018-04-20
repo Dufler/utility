07526584936688a73441efe37f09ceacb9ba6bf6
Damiano <Damiano@Damiano-PC>
2018-04-20 11:46:09 +0200

    Progetto che utile ad interfacciarsi con siti FTP.

---
+++ it.ltc.utility.ftp/.svn/pristine/ab/ab1371bb5208f08147e5893f912ab7783db728ee.svn-base
@@ -0,0 +1,165 @@
+package it.ltc.utility.ftp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.ChannelSftp.LsEntry;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpATTRS;
+import com.jcraft.jsch.SftpException;
+
+public class SFTP {
+	
+	private final static int PORT = 22;
+
+	private final JSch jsch;
+	private final String host;
+	private final String username;
+	private final String password;
+	
+	private Session session;
+	
+	public SFTP(String pathToHost, String user, String pwd) {
+		jsch = new JSch();
+		host = pathToHost;
+		username = user;
+		password = pwd;
+		
+	}
+	
+	public boolean connect() {
+		boolean connesso = false;
+		try {
+			session = jsch.getSession(username, host, PORT);
+			session.setConfig("StrictHostKeyChecking", "no");
+			session.setPassword(password);
+			session.connect();
+			connesso = true;
+		} catch (JSchException e) {
+			session = null;
+			e.printStackTrace();
+		}
+		return connesso;
+	}
+	
+	public boolean disconnect() {
+		boolean disconnesso = false;
+		if (session != null) {
+			session.disconnect();
+			disconnesso = true;
+		}		
+		return disconnesso;
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<String> getFileList(String path) {
+		ArrayList<String> files = new ArrayList<String>();
+		if (session == null) {
+			connect();
+		}
+		try {
+			Channel channel = session.openChannel("sftp");
+		    channel.connect();
+		    ChannelSftp sftpChannel = (ChannelSftp) channel;
+		    Vector<LsEntry> names = sftpChannel.ls(path);
+		    for (LsEntry entry : names) {
+		    	SftpATTRS attributi = entry.getAttrs();
+		    	if (!attributi.isDir())
+		    		files.add(entry.getFilename());
+		    }
+		    sftpChannel.exit();
+		} catch (JSchException | SftpException e) {
+			e.printStackTrace();
+		}
+		return files;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<String> getFolderList(String path) {
+		ArrayList<String> files = new ArrayList<String>();
+		if (session == null) {
+			connect();
+		}
+		try {
+			Channel channel = session.openChannel("sftp");
+		    channel.connect();
+		    ChannelSftp sftpChannel = (ChannelSftp) channel;
+		    Vector<LsEntry> names = sftpChannel.ls(path);
+		    for (LsEntry entry : names) {
+		    	SftpATTRS attributi = entry.getAttrs();
+		    	if (attributi.isDir())
+		    		files.add(entry.getFilename());
+		    }
+		    sftpChannel.exit();
+		} catch (JSchException | SftpException e) {
+			e.printStackTrace();
+		}
+		return files;
+	}
+	
+	public boolean download(String remotePath, String localPath) {
+		boolean download = false;
+		if (session == null) {
+			connect();
+		}
+		try {
+			Channel channel = session.openChannel("sftp");
+		    channel.connect();
+		    ChannelSftp sftpChannel = (ChannelSftp) channel;
+		    sftpChannel.get(remotePath, localPath);
+		    download = true;
+		    sftpChannel.exit();
+		} catch (JSchException | SftpException e) {
+			e.printStackTrace();
+		}
+		return download;
+	}
+	
+	/**
+	 * Consente l'upload di un file.
+	 * @param remotePath il percorso che il file dovrà avere sul server SFTP
+	 * @param localPath il percorso del file da uploadare
+	 * @return un boolean che indica il successo dell'operazione
+	 */
+	public boolean upload(String localPath, String remotePath) {
+		boolean upload = false;
+		if (session == null) {
+			connect();
+		}
+		try {
+			Channel channel = session.openChannel("sftp");
+		    channel.connect();
+		    ChannelSftp sftpChannel = (ChannelSftp) channel;
+		    sftpChannel.put(localPath, remotePath);
+		    upload = true;
+		    sftpChannel.exit();
+		} catch (JSchException | SftpException e) {
+			e.printStackTrace();
+		}
+		return upload;
+	}
+	
+	public boolean rename(String remotePath, String newName) {
+		boolean rename = false;
+		if (session == null) {
+			connect();
+		}
+		try {
+			Channel channel = session.openChannel("sftp");
+		    channel.connect();
+		    ChannelSftp sftpChannel = (ChannelSftp) channel;
+		    sftpChannel.rename(remotePath, newName);
+		    rename = true;
+		} catch (JSchException | SftpException e) {
+			e.printStackTrace();
+		}
+		return rename;
+	}
+	
+}
---
+++ it.ltc.utility.ftp/src/main/java/it/ltc/utility/ftp/FTP.java
@@ -0,0 +1,226 @@
+package it.ltc.utility.ftp;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.log4j.Logger;
+
+/**
+ * Classe che offre semplici funzionalità FTP.
+ *
+ * @author Damiano Bellucci - damiano.bellucci@gmail.com
+ * @version 1.0
+ *
+ */
+public class FTP {
+	
+	private static final Logger logger = Logger.getLogger("FTP");
+	
+	private final String host, username, password;
+	
+	private final FTPClient ftpClient;
+	
+	/**
+	 * Costruisce un connettore FTP per l'upload o il download di file.
+	 * @param path il sito FTP
+	 * @param user il nume utente da utilizzare, lasciare vuoto o null per accessi anonimi
+	 * @param pwd la password da utilizzare, lasciare vuoto o null per accessi anonimi
+	 */
+	public FTP(String path, String user, String pwd) {
+		if (path.endsWith("/"))
+			path = path.substring(0, path.length() - 1);
+		host = path;
+		username = user;
+		password = pwd;
+		ftpClient = new FTPClient();
+		login();
+	}
+	
+	private boolean login() {
+		boolean login = true;
+		if (!ftpClient.isConnected()) {
+			try {
+				ftpClient.connect(host);
+				login = ftpClient.login(username, password);
+			} catch (IOException e) {
+				login = false;
+				logger.error(e);
+			}
+		}
+		return login;
+	}
+	
+	private void logout() {
+		if (ftpClient.isConnected()) {
+			try {
+				ftpClient.disconnect();
+			} catch (IOException e) {
+				logger.error(e);
+			}
+		}
+	}
+	
+	/**
+	 * Restituisce tutti i nomi dei file nel path relativo specificato
+	 * @param remoteRelativePath il percorso relativo all'interno del server FTP
+	 * @return una lista di stringhe che rappresentano i nomi dei file
+	 */
+	public List<String> getFiles(String remoteRelativePath) {
+		LinkedList<String> contenuto = new LinkedList<String>();
+		login();
+		try {			
+			FTPFile[] files = ftpClient.listFiles(remoteRelativePath);
+			for (FTPFile file : files) {
+				if (file.getType() == FTPFile.FILE_TYPE)
+					contenuto.add(file.getName());
+			}
+		}  catch (IOException e) {
+			logger.error(e);
+        }
+		logout();
+		return contenuto;
+	}
+	
+	/**
+	 * Restituisce tutti i nomi delle sottocartelle nel path relativo specificato
+	 * @param remoteRelativePath il percorso relativo all'interno del server FTP
+	 * @return una lista di stringhe che rappresentano i nomi delle sottocartelle
+	 */
+	public List<String> getFolders(String remoteRelativePath) {
+		LinkedList<String> contenuto = new LinkedList<String>();
+		login();
+		try {
+			FTPFile[] files = ftpClient.listFiles(remoteRelativePath);
+			for (FTPFile file : files) {
+				if (file.getType() == FTPFile.DIRECTORY_TYPE)
+					contenuto.add(file.getName());
+			}
+		}  catch (IOException e) {
+			//e.printStackTrace();
+			logger.error(e);
+        }
+		logout();
+		return contenuto;
+	}
+	
+	/**
+	 * Esegue l'upload di un file locale sul percorso relativo specificato
+	 * @param localPath il percorso in locale del file da 'uploadare'
+	 * @param remoteRelativePath il percorso relativo (cartella + nome) che avrà il file sul sito FTP
+	 * @return l'esito dell'operazione.
+	 */
+	public boolean upload(String localPath, String remoteRelativePath) {
+		boolean upload = true;
+		login();
+		try {
+			FileInputStream inputStream = new FileInputStream(localPath);
+		    upload = ftpClient.storeFile(remoteRelativePath, inputStream);
+		    inputStream.close();
+		} catch (IOException e) {
+			upload = false;
+			//e.printStackTrace();
+			logger.error(e);
+        }
+		logout();
+		return upload;
+	}
+	
+	/**
+	 * Esegue il download di un file dal percorso relativo specificato sul percorso locale desiderato
+	 * @param localPath il percorso locale dove verrà salvato il file
+	 * @param remoteRelativePath il percorso della risorsa remota da scaricare
+	 * @return l'esito dell'operazione
+	 */
+	public boolean download(String localPath, String remoteRelativePath) {
+		boolean download = true;
+		login();
+		try {
+			FileOutputStream outputStream = new FileOutputStream(localPath);
+			download = ftpClient.retrieveFile(remoteRelativePath, outputStream);
+		} catch (IOException e) {
+			download = false;
+			//e.printStackTrace();
+			logger.error(e);
+        }
+		logout();
+		return download;
+	}
+	
+	public boolean rename(String from, String to) {
+		boolean rename = true;
+		login();
+		try {
+			ftpClient.rename(from, to);
+		} catch (IOException e) {
+			rename = false;
+			//e.printStackTrace();
+			logger.error(e);
+        }
+		logout();
+		return rename;
+		
+	}
+	
+	/**
+	 * Cancella il file specificato sul server FTP
+	 * @param remoteRelativePath il percorso della risorsa remota da cancellare
+	 * @return l'esito dell'operazione
+	 */
+	public boolean deleteFile(String remoteRelativePath) {
+		boolean delete = true;
+		login();
+		try {
+			delete = ftpClient.deleteFile(remoteRelativePath);
+		} catch (IOException e) {
+			delete = false;
+			//e.printStackTrace();
+			logger.error(e);
+		}
+		logout();
+		return delete;
+	}
+	
+	/**
+	 * Crea una cartella con il percorso specificato sul server FTP
+	 * @param remoteRelativePath il percorso in cui creare la cartella
+	 * @return l'esito dell'operazione
+	 */
+	public boolean createFolder(String remoteRelativePath) {
+		boolean create = true;
+		login();
+		try {
+			create = ftpClient.makeDirectory(remoteRelativePath);
+		} catch (IOException e) {
+			create = false;
+			//e.printStackTrace();
+			logger.error(e);
+		}
+		logout();
+		return create;
+	}
+	
+	/**
+	 * Cancella la cartella sul percorso specificato
+	 * @param remoteRelativePath il percorso relativo della cartella da cancellare sul server FTP
+	 * @return l'esito dell'operazione
+	 */
+	public boolean deleteFolder(String remoteRelativePath) {
+		boolean delete = true;
+		login();
+		try {
+			delete = ftpClient.removeDirectory(remoteRelativePath);
+		} catch (IOException e) {
+			delete = false;
+			//e.printStackTrace();
+			logger.error(e);
+		}
+		logout();
+		return delete;
+	}
+
+}
